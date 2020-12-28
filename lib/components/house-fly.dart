efbca0ed9f1a51b481dff8c536e33427aa718749
Betrayer <StarDestroyer@ukr.net>
2020-12-28 11:54:10 +0200

    added test full sprite and high score

--- lib/components/house-fly.dart
+++ lib/components/house-fly.dart
@@ -8,8 +8,32 @@ class HouseFly extends Fly {
   HouseFly(BetrayerGame game, double x, double y) : super(game) {
     flyRect = Rect.fromLTWH(x, y, game.tileSize * 1.5, game.tileSize * 1.5);
     flyingSprite = List<Sprite>();
-    flyingSprite.add(Sprite('flies/028-poker-face.png'));
-    flyingSprite.add(Sprite('flies/037-angry.png'));
+    // INITIAL FLIES
+    // flyingSprite.add(Sprite('flies/028-poker-face.png'));
+    // flyingSprite.add(Sprite('flies/037-angry.png'));
+
+    // ADVANCED FLIES
+    // flyingSprite.add(Sprite('flies/fly_1.png'));
+    // flyingSprite.add(Sprite('flies/fly_2.png'));
+    flyingSprite.add(Sprite('flies/fly_3.png'));
+    flyingSprite.add(Sprite('flies/fly_4.png'));
+    flyingSprite.add(Sprite('flies/fly_5.png'));
+    flyingSprite.add(Sprite('flies/fly_6.png'));
+    flyingSprite.add(Sprite('flies/fly_7.png'));
+    flyingSprite.add(Sprite('flies/fly_8.png'));
+    flyingSprite.add(Sprite('flies/fly_9.png'));
+    flyingSprite.add(Sprite('flies/fly_10.png'));
+    flyingSprite.add(Sprite('flies/fly_11.png'));
+    // flyingSprite.add(Sprite('flies/fly_12.png'));
+    // flyingSprite.add(Sprite('flies/fly_13.png'));
+    // flyingSprite.add(Sprite('flies/fly_14.png'));
+    // flyingSprite.add(Sprite('flies/fly_15.png'));
+    // flyingSprite.add(Sprite('flies/fly_16.png'));
+    // flyingSprite.add(Sprite('flies/fly_17.png'));
+    // flyingSprite.add(Sprite('flies/fly_18.png'));
+    // flyingSprite.add(Sprite('flies/fly_19.png'));
+    // flyingSprite.add(Sprite('flies/fly_20.png'));
+
     deadSprite = Sprite('flies/015-dead.png');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,13 @@ import 'package:flutter/gestures.dart';
 import 'package:flame/flame.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  SharedPreferences storage = await SharedPreferences.getInstance();
-  BetrayerGame game = BetrayerGame(storage);
-  runApp(game.widget);
-
-
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Util flameUtil = Util();
   flameUtil.fullScreen();
   flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  SharedPreferences storage = await SharedPreferences.getInstance();
 
   Flame.images.loadAll(<String>[
     'bg/bg.jpg',
@@ -25,6 +22,26 @@ void main() {
     'flies/050-angry.png',
     'flies/046-crazy.png',
     'flies/028-poker-face.png',
+    'flies/fly_1.png',
+    'flies/fly_2.png',
+    'flies/fly_3.png',
+    'flies/fly_4.png',
+    'flies/fly_5.png',
+    'flies/fly_6.png',
+    'flies/fly_7.png',
+    'flies/fly_8.png',
+    'flies/fly_9.png',
+    'flies/fly_10.png',
+    'flies/fly_11.png',
+    'flies/fly_12.png',
+    'flies/fly_13.png',
+    'flies/fly_14.png',
+    'flies/fly_15.png',
+    'flies/fly_16.png',
+    'flies/fly_17.png',
+    'flies/fly_18.png',
+    'flies/fly_19.png',
+    'flies/fly_20.png',
     'menu/f.jpg',
     'menu/goal.jpg',
     'menu/handshake.png',
@@ -34,6 +51,9 @@ void main() {
     'menu/start.png',
     'menu/speech-bubble.png',
   ]);
+
+  BetrayerGame game = BetrayerGame(storage);
+  runApp(game.widget);
 
   TapGestureRecognizer tapper = TapGestureRecognizer();
   tapper.onTapDown = game.onTapDown;
